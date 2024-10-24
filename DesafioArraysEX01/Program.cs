ba16c8bf444774d4c8ece6e5ca47b7d02180dda1
GUILHERME GONÇALVES LIMA <54261396874@info.local>
2024-10-24 16:52:48 -0300

    arrays

---
+++ DesafioArraysEX01/Program.cs
@@ -0,0 +1,8 @@
+﻿int[] numeros = new int[6]
+for (int i = 0; if <numeros.Length; i++)
+Console.WriteLine($"Digite um {i + 1}º numero");
+int numeroDigitado = int.Parse(Console.ReadLine()!);
+if (numeroDigitado % 2 ==0)
+    {
+        Par++;
+    }
---
+++ arrays/Program.cs
@@ -0,0 +1,21 @@
+﻿
+//  Console.WriteLine($"Digite o nome do carro a ser cadastrado");
+//  carros [1] = Console.ReadLine()!;
+
+//  Console.WriteLine($"Digite o nome do carro a ser cadastrado");
+//  carros [2]= Console.ReadLine()!;
+
+
+
+
+
+string[] carros = new string[3];
+
+
+for (int i = 0; i < carros.Length; i++)
+{
+    Console.WriteLine($"Digite o nome do carro a ser cadastrado");
+    carros[i] = Console.ReadLine()!;
+}
+
+Console.WriteLine($"Os carros cadastrados são: {carros[0]}, {carros[1]}, {carros[2]}");
